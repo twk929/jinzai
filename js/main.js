db5912517d30db6083127458c0983134f5f17922
THARINDU　タリンドゥ <84095830+twk929@users.noreply.github.com>
2022-09-14 11:50:47 +0900

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,7 +1,8 @@
 window.onscroll = () => {
     toggleTopButton();
 }
-function scrollToTop(){
+
+function scrollToTop() {
     window.scrollTo({top: 0, behavior: 'smooth'});
 }
 
@@ -13,5 +14,59 @@ function toggleTopButton() {
         document.getElementById('back-to-up').classList.add('d-none');
     }
 }
+
+
+$('.count').each(function () {
+    $(this).prop('Counter', 0).animate({
+        Counter: $(this).text()
+    }, {
+        duration: 8000,
+        easing: 'swing',
+        step: function (now) {
+            $(this).text(Math.ceil(now));
+        }
+    });
+});
+
+var a = 0;
+$(window).scroll(function () {
+    var oTop = $("#counter-box").offset().top - window.innerHeight;
+    if (a == 0 && $(window).scrollTop() > oTop) {
+        $(".counter").each(function () {
+            var $this = $(this),
+                countTo = $this.attr("data-number");
+            $({
+                countNum: $this.text()
+            }).animate(
+                {
+                    countNum: countTo
+                },
+
+                {
+                    duration: 5000,
+                    easing: "swing",
+                    step: function () {
+                        //$this.text(Math.ceil(this.countNum));
+                        $this.text(
+                            Math.ceil(this.countNum).toLocaleString("en")
+                        );
+                    },
+                    complete: function () {
+                        $this.text(
+                            Math.ceil(this.countNum).toLocaleString("en")
+                        );
+                        //alert('finished');
+                    }
+                }
+            );
+        });
+        a = 1;
+    }
+});
+
+
+
+
+
 
 
